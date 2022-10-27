d3b9ec56d930503fa32711dda6b2862e3a052462
nich-dev <nich.dev95@gmail.com>
2022-10-27 10:57:51 -0300

    Migración al sidebar component

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,6 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  styles: [
+    `
+    /* esto le dice al html que los li de este componente  van a tener el cursor en pointer */
+      li {
+        cursor: pointer;
+      }
+    `
+  ]
 })
 export class SidebarComponent {
 
