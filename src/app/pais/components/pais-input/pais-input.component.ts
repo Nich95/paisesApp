5807c89745de7bab0109fbedefcdd35809b319aa
nich-dev <nich.dev95@gmail.com>
2022-11-07 11:33:17 -0300

    Traspaso de info

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pais-input',
@@ -6,10 +6,16 @@ import { Component } from '@angular/core';
 })
 export class PaisInputComponent {
 
+  // a los eventos se les suele poner la palabra on antess para describir que es un evento
+  // es importante especificar el tipo de evento que se va a emitir, 
+  // en este caso el evento que se va a emitir es termino y el termino es de tipo string
+  @Output() onEnter: EventEmitter<string> = new EventEmitter();
+
   termino: string = '';
 
   buscar() {
-    console.log('hola mundo' );
-    console.log( this.termino );
+    // la siguiente linea llama al evento onEnter que fue inicializado como EvenEmitter
+    // y como argumento se le entrega el termino que queremos enviar
+    this.onEnter.emit( this.termino );
   }
 }
