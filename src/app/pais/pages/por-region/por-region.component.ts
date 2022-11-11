582fb18f95f6333bf303e5dce0412d56eee6f70f
nich-dev <nich.dev95@gmail.com>
2022-11-11 20:34:56 -0300

    Se arma el esqueleto de la vista

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styleUrls: ['./por-region.component.css']
+  styles:[`
+   /* este css se aplica unicamente a este componente, en este caso a los botones */
+    button {
+      margin-right: 5px;
+    }
+  `]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  activarRegion( region: string ) {
+    this.regionActiva = region;
 
+    // TODO: hacer el llamado al servicio
+  }
 }
