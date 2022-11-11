672bade9f106e66f7ef4553c2139407586e69d8c
nich-dev <nich.dev95@gmail.com>
2022-11-11 13:31:20 -0300

    Pantalla ver país

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators'; // permite ingresar un obsavable y regresa un observable
+import { switchMap, tap } from 'rxjs/operators'; // permite ingresar un obsavable y regresa un observable
 
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -10,19 +11,21 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais: Country[] = [] // le dice a typescript que confie en lo que hago y tratalo como situviera data aun que sea null
+  
   constructor( 
     private activatedRoute: ActivatedRoute,
     private paisService: PaisService
-    ) { } // el constrcutor actua antes de que el componente este inicializado
+    ) {  } // el constrcutor actua antes de que el componente este inicializado
 
   ngOnInit(): void { // esta sección es cuando el componente ya esta inicializado 
     this.activatedRoute.params
-    .pipe(
-      switchMap( ({ id }) => this.paisService.getPaisPorAlpha( id ) ) // desestructuración de argumentos
-    )  
-    .subscribe( resp => {
-        console.log( resp );
-      } );
+      .pipe(
+        switchMap( ({ id }) => this.paisService.getPaisPorAlpha( id ) ), // desestructuración de argumentos
+        tap( console.log ) // el tap recibe el producto del observable lo imprime en consola
+      )
+      .subscribe( pais => this.pais = pais );
+
     // this.activatedRoute.params
     //   .subscribe( ({ id }) => {
     //     console.log( id );
