6d741017826df9371fdd87bda8f12001d56d9996
Nich <116022893+Nich95@users.noreply.github.com>
2022-11-12 23:39:20 -0300

    Se muestran los pasises (#22)

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ROUTER_CONFIGURATION } from '@angular/router';
+
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -14,16 +18,25 @@ export class PorRegionComponent {
 
   regiones: string[] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC'];
   regionActiva: string = '';
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor( private paisservice: PaisService ) { }
 
   gerClaseCSS( region: string ): string {
     return (region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
   }
 
   activarRegion( region: string ) {
-    this.regionActiva = region;
+    if ( region === this.regionActiva ) { return; } // si esto se cumple el metodo llega hasta aqui
 
-    // TODO: hacer el llamado al servicio
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.paisservice.buscarRegion( this.regionActiva )
+      .subscribe( ( paises ) => {
+        this.paises = paises;
+      }, ( err ) => {
+        this.paises = [];
+      } )
   }
 }
