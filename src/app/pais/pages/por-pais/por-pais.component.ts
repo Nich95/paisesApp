de0585d644fc50fa0d5a61d224c30fb9790bf07a
Nich <116022893+Nich95@users.noreply.github.com>
2022-11-18 11:55:27 -0300

    Sugerencias de busqueda (#24)

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -4,17 +4,28 @@ import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html'
+  templateUrl: './por-pais.component.html',
+  styles: [
+    `
+      li {
+        /* sirve para que el cursor cambie para que se pueda hacer click */
+        cursor: pointer;
+      }
+    `
+  ]
 })
 export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor( private paisService: PaisService ) {}
 
   buscar( termino: string ) {
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino; // aqui se le dice que el this.terimno es igual al termino que se recibe como argumento 
 
@@ -30,6 +41,17 @@ export class PorPaisComponent {
 
   sugerencias( termino: string ) {
     this.hayError = false;
-    // TODO: crear sugerencias
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais( termino )
+      .subscribe( 
+        paises => this.paisesSugeridos = paises.splice(0,4),
+        (err) => this.paisesSugeridos = [] // con esto quitamos el error al buscar un termino que no existe
+      );
+  }
+
+  buscarSugerencias( termino: string ) {
+    this.buscar( termino );
   }
 }
